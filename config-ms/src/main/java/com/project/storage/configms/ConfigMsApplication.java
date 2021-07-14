0ae3d84d35e203ff3466df3f50aa96b3b9c6db80
julio-svg <julioangel.hernandez@gmail.com>
2021-07-14 09:21:05 +0200

    First version of config-server

---
+++ config-ms/src/main/java/com/project/storage/configms/ConfigMsApplication.java
@@ -0,0 +1,15 @@
+package com.project.storage.configms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigMsApplication.class, args);
+	}
+
+}
---
+++ config-ms/src/test/java/com/project/storage/configms/ConfigMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.storage.configms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
